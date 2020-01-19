03563e60db745a511f5ae50398e0f1f123bc063c
Eugen Kleinschmidt <10838796+eugenkleinschmidt@users.noreply.github.com>
2020-01-19 02:01:55 +0100

    include tabs in gui

--- src/songdb_gui/songdb_gui.py
+++ src/songdb_gui/songdb_gui.py
@@ -6,15 +6,35 @@ from songdb.songdb import SongDB as sdb
 
 
 class SongDbGui(object):
+    PARAM_FILE = '.gui.params'
+
     def __init__(self):
 
         new_song_layout = [
-            [sg.Text('Songname'), sg.Input(key='i_songname')],
+            [sg.Text('Song Name'), sg.Input(key='i_songname')],
             [sg.Text('Link'), sg.Input(key='i_link')],
-            [sg.Button('Enter'), sg.Exit()]
+            [sg.Text('Sheet'), sg.Input(key='i_sheet')],
+            [sg.Button('Enter')]
         ]
 
-        window = sg.Window('Song DB', new_song_layout)
+        new_setlist_layout = [
+            [sg.Text('Setlist Name'), sg.Input(key='i_setlist')],
+            [sg.Text('Date'), sg.Input(key='i_date')],
+            [sg.Text('Song'), sg.Input(key='i_song')],
+            [sg.Button('Enter')]
+        ]
+
+        layout = [
+            [sg.TabGroup([[
+                sg.Tab('Songs', new_song_layout),
+                sg.Tab('Setlist', new_setlist_layout)
+                ]]
+            )],
+            [sg.Button('New Song'), sg.Exit()]
+        ]
+
+        window = sg.Window('Song DB', layout, finalize=True)
+        window.load_from_disk(self.PARAM_FILE)
 
         while True:  # The Event Loop
             event, values = window.read()
@@ -26,6 +46,7 @@ class SongDbGui(object):
             if event in (None, 'Exit'):
                 break
 
+        window.save_to_disk(self.PARAM_FILE)
         window.close()
 
 
